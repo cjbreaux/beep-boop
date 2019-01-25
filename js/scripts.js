ae39fc19a32c03eef5d01372b9547b987b50f4db
Chris Breaux <christopherjbreaux@gmail.com>
2019-01-25 11:44:07 -0800

    begin to refactor code to use an array of strings instead of integers

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,30 @@
 //this will create a for loop to produce an array doing the same thing as the previous code but for any number.
 var array = [];
 var resultArray = [];
+var resultString = "";
 
-function toArray(number) {
+function counter (number) {
   for(i=0; i<=number; i+=1) {
   array.push(number - i);
   }
   array = array.reverse();
+  resultString = array.join();
+  array = resultString.split();
   console.log(array);
-  return array
+  return array;
 }
 
 function beepBooper(number) {
-  var newArray = toArray(number);
-  console.log("Inside beepBooper" + newArray);
-  newArray.forEach(function(num){
-    if (num === 1) {
-      resultArray.push("Beep!");
-    } else if (num === 2) {
-      resultArray.push("Boop!")
-    } else if (num === 3) {
-      resultArray.push("I'm sorry Dave. I'm afraid I can't do that.")
-    } else {
-      resultArray.push(num);
+  var outputString = counter(number);
+  //console.log("Inside beepBooper" + outputString);
+
+  for (i=0; i<outputString.length; i+=1) {
+    console.log(outputString[i]);
+    if (outputString[i].includes("1")) {
+      //alert("has a 1");
     }
-  }) //end forEach loop
+  }
+ console.log(outputString);
 }; //end beepBooper
 
 //beginning to implement user interface logic to display results to index.html
@@ -34,11 +34,11 @@ $(document).ready(function(){
   $("form#beepbooper").submit(function(event){
     event.preventDefault();
     var number = parseInt($("input#userInput").val());
-    console.log(number);
+  //  console.log(number);
     //store the result as a new variable * not needed as of yet
     var result = beepBooper(number);
-    console.log(number);
+  //  console.log(number);
     //print the populated array
-    $("#results").text(resultArray);
+    $("#results").text(resultString);
   });
 });
