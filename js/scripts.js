5a46bd66b154c9a5a0fe8bc5c6649927565f7629
Chris Breaux <christopherjbreaux@gmail.com>
2019-01-25 09:03:11 -0800

    begin to contruct business logic for the program

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//test script that will take in 5 and return an array starting from 0 and counting up 5
+//this will help to see what needs to happen during a loop
+
+var number = 5;
+var array = [];
+
+
+array.push(number);
+array.push(number-1);
+array.push(number-2);
+array.push(number-3);
+array.push(number-4);
+array.push(number-number)
+console.log(array.reverse());
